6242a2c04c4a404d6b9b999f2afb5e2e6e54f4e6
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-06-27 23:40:51 +0200

    Add deps

--- pythonforandroid/recipes/pandas/__init__.py
+++ pythonforandroid/recipes/pandas/__init__.py
@@ -6,7 +6,10 @@ class PandasRecipe(CythonRecipe):
     url = 'https://github.com/pandas-dev/pandas/archive/{version}.zip'
     name = 'pandas'
 
-    depends = [('python2', 'python3crystax')]
+    depends = [
+        ('python2', 'python3crystax'),
+        'numpy', 'pytz', 'python-dateutil'
+    ]
 
 
 recipe = PandasRecipe()
