95e9b4393108918d1aa88626e2f82ce9f4339899
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-06-27 23:40:36 +0200

    Add bones for pandas

---
+++ pythonforandroid/recipes/pandas/__init__.py
@@ -0,0 +1,12 @@
+from pythonforandroid.toolchain import CythonRecipe
+
+
+class PandasRecipe(CythonRecipe):
+    version = 'v0.20.2'
+    url = 'https://github.com/pandas-dev/pandas/archive/{version}.zip'
+    name = 'pandas'
+
+    depends = [('python2', 'python3crystax')]
+
+
+recipe = PandasRecipe()
